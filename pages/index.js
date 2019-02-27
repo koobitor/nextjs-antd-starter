eb2360fcc5e1abef538ac39a4b9ac01b1f7522db
Sakol Assawasagool <koobitor@gmail.com>
2019-02-27 14:44:17 +0700

    import button example

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
 import Layout from '../components/Layout'
+import { Button } from 'antd';
 
 export default () => (
   <Layout>
     <div className="container">
       <div>Hello World.</div>
+      <div>
+        <Button type="primary">Primary</Button>
+        <Button>Default</Button>
+        <Button type="dashed">Dashed</Button>
+        <Button type="danger">Danger</Button>
+      </div>
     </div>
   </Layout>
 )
